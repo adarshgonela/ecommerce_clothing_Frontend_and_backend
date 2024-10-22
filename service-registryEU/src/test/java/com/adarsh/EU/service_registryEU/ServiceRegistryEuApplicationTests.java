347d46bf6da7495b8320ad8a682ae3b83e4fcc02
adarshgonela <adarshgonela@gmail.com>
2024-10-22 13:06:55 +0530

    EUREKA SERVER(SERVICE - REGISTRY ) ADDED

---
+++ service-registryEU/src/test/java/com/adarsh/EU/service_registryEU/ServiceRegistryEuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adarsh.EU.service_registryEU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryEuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
